8cd10bf7a15accbfd96ea7530677fec1de8c0345
jhonasrn <jhonas.rn@gmail.com>
2015-09-29 12:25:19 -0300

    conflito

--- src/classes/entity/amostra.php
+++ src/classes/entity/amostra.php
@@ -6,6 +6,7 @@
 	 */
 	class Amostra{
 		
+		private $id_amostra
 		private $fosforo;
 		private $materia_organica;
 		private $potencial_hidrogenionico;
@@ -23,6 +24,71 @@
 		private $argila;
 		private $silte;
 		private $areia;
+
+
+		public function getCalcio(){
+			return $this->calcio;
+		}
+
+		public function setCalcio($calcio){
+			$this->calcio = $calcio;
+		}
+		
+		public function getMagnesio(){
+			return $this->magnesio;
+		}
+
+		public function setMagnesio($magnesio){
+			$this->magnesio = $magnesio;
+		}
+
+		public function getCalcio(){
+			return $this->calcio;
+		}
+
+		public function setCalcio($calcio){
+			$this->calcio = $calcio;
+		}
+
+		public function getFosforo(){
+			return $this->fosforo;
+		}
+
+		public function setFosforo($fosforo){
+			$this->fosforo = $fosforo;
+		}
+
+		public function getMateria_organica(){
+			return $this->materia_organica;
+		}
+		
+		public function setMateria_organica($materia_organica){
+			$this->materia_organica = $materia_organica;
+		}
+
+		public function getPotencial_hidrogenionico(){
+			return $this->potencial_hidrogenionico;
+		}
+		
+		public function setPotencial_hidrogenionico($potencial_hidrogenionico){
+			$this->potencial_hidrogenionico = $potencial_hidrogenionico;
+		}
+
+		public function getPotassio(){
+			return $this->potassio;
+		}
+
+		public function setPotassio($potassio){
+			$this->potassio = $potassio;
+		}
+
+		public function getPotassio(){
+			return $this->potassio;
+		}
+
+		public function setPotassio($potassio){
+			$this->potassio = $potassio;
+		}
 	}
 
 ?>
