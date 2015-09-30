18d540253e4ff33adc8948516b377a7ac15b5601
jhonasrn <jhonas.rn@gmail.com>
2015-09-30 12:52:02 -0300

    metodos get e set

--- src/classes/entity/amostra.php
+++ src/classes/entity/amostra.php
@@ -6,7 +6,7 @@
 	 */
 	class Amostra{
 		
-		private $id_amostra
+		private $id_amostra;
 		private $fosforo;
 		private $materia_organica;
 		private $potencial_hidrogenionico;
@@ -24,14 +24,93 @@
 		private $argila;
 		private $silte;
 		private $areia;
+                
+                public function getId_amostra(){
+                    return $this->id_amostra;
+                }
+                
+                public function setId_amostra($id_amostra){
+                    $this->id_amostra = $id_amostra;
+                }
+                
+                public function getAreia(){
+                    return $this->areia;
+                }
+                
+                public function setAreia($areia){
+                    $this->areia = $areia;
+                }
+                
+                public function getSilte(){
+                    return $this->silte;
+                }
+                
+                public function setSilte($silte){
+                    $this->silte = $silte;
+                }
+                
+                public function getArgila(){
+                    return $this->argila;
+                }
+                
+                public function setArgila($argila){
+                    $this->argila = $argila;
+                }
+                
+                public function getAluminio(){
+                    return $this->aluminio;
+                }
+                
+                public function setAluminio($aluminio){
+                    $this->aluminio = $aluminio;
+                }
+                
+                public function getSodio(){
+                    return $this->sodio;
+                }
+                
+                public function setSodio($sodio){
+                    $this->sodio = $sodio;
+                }
+                
+                public function getZinco(){
+                    return $this->zinco;
+                }
+                
+                public function setZinco($zinco){
+                    $this->zinco = $zinco;
+                }
+                
+                public function getMaganes(){
+                    return $this->maganes;
+                }
+                
+                public function setMaganes($maganes){
+                    $this->maganes = $maganes;
+                }
+                
+                public function getFerro(){
+                    return $this->ferro;
+                }
+                
+                public function setFero($ferro){
+                    $this->ferro = $ferro;
+                }
+                
+                public function getCobre(){
+                    return $this->cobre;
+                }
+                
+                public function setCobre($cobre){
+                    $this->cobre = $cobre;
+                }
 
-
-		public function getCalcio(){
+                public function getAcidez_total(){
 			return $this->calcio;
 		}
 
-		public function setCalcio($calcio){
-			$this->calcio = $calcio;
+		public function setAcidez_total($acidez_total){
+			$this->acidez_total = $acidez_total;
 		}
 		
 		public function getMagnesio(){
@@ -82,13 +161,13 @@
 			$this->potassio = $potassio;
 		}
 
-		public function getPotassio(){
+		public function getBoro(){
 			return $this->potassio;
 		}
 
-		public function setPotassio($potassio){
-			$this->potassio = $potassio;
+		public function setBoro($boro){
+			$this->boro = $boro;
 		}
-	}
+}
 
 ?>
