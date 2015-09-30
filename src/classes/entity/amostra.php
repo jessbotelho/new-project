7a8d25adab1f8219a3e7c56d4c058e02d47c8c79
jhonasrn <jhonas.rn@gmail.com>
2015-09-30 13:12:10 -0300

    identação

--- src/classes/entity/amostra.php
+++ src/classes/entity/amostra.php
@@ -1,173 +1,174 @@
 <?php
-	
-	/**
-	 * @author Jhonas
-	 *
-	 */
-	class Amostra{
-		
-		private $id_amostra;
-		private $fosforo;
-		private $materia_organica;
-		private $potencial_hidrogenionico;
-		private $potassio;
-		private $calcio;
-		private $magnesio;
-		private $acidez_total;
-		private $boro;
-		private $cobre;
-		private $ferro;
-		private $manganes;
-		private $zinco;
-		private $sodio;
-		private $aluminio;
-		private $argila;
-		private $silte;
-		private $areia;
-                
-                public function getId_amostra(){
-                    return $this->id_amostra;
-                }
-                
-                public function setId_amostra($id_amostra){
-                    $this->id_amostra = $id_amostra;
-                }
-                
-                public function getAreia(){
-                    return $this->areia;
-                }
-                
-                public function setAreia($areia){
-                    $this->areia = $areia;
-                }
-                
-                public function getSilte(){
-                    return $this->silte;
-                }
-                
-                public function setSilte($silte){
-                    $this->silte = $silte;
-                }
-                
-                public function getArgila(){
-                    return $this->argila;
-                }
-                
-                public function setArgila($argila){
-                    $this->argila = $argila;
-                }
-                
-                public function getAluminio(){
-                    return $this->aluminio;
-                }
-                
-                public function setAluminio($aluminio){
-                    $this->aluminio = $aluminio;
-                }
-                
-                public function getSodio(){
-                    return $this->sodio;
-                }
-                
-                public function setSodio($sodio){
-                    $this->sodio = $sodio;
-                }
-                
-                public function getZinco(){
-                    return $this->zinco;
-                }
-                
-                public function setZinco($zinco){
-                    $this->zinco = $zinco;
-                }
-                
-                public function getMaganes(){
-                    return $this->maganes;
-                }
-                
-                public function setMaganes($maganes){
-                    $this->maganes = $maganes;
-                }
-                
-                public function getFerro(){
-                    return $this->ferro;
-                }
-                
-                public function setFero($ferro){
-                    $this->ferro = $ferro;
-                }
-                
-                public function getCobre(){
-                    return $this->cobre;
-                }
-                
-                public function setCobre($cobre){
-                    $this->cobre = $cobre;
-                }
 
-                public function getAcidez_total(){
-			return $this->calcio;
-		}
+/**
+ * @author Jhonas
+ *
+ */
+class Amostra {
 
-		public function setAcidez_total($acidez_total){
-			$this->acidez_total = $acidez_total;
-		}
-		
-		public function getMagnesio(){
-			return $this->magnesio;
-		}
+    private $id_amostra;
+    private $fosforo;
+    private $materia_organica;
+    private $potencial_hidrogenionico;
+    private $potassio;
+    private $calcio;
+    private $magnesio;
+    private $acidez_total;
+    private $boro;
+    private $cobre;
+    private $ferro;
+    private $manganes;
+    private $zinco;
+    private $sodio;
+    private $aluminio;
+    private $argila;
+    private $silte;
+    private $areia;
 
-		public function setMagnesio($magnesio){
-			$this->magnesio = $magnesio;
-		}
+    public function getId_amostra() {
+        return $this->id_amostra;
+    }
 
-		public function getCalcio(){
-			return $this->calcio;
-		}
+    public function setId_amostra($id_amostra) {
+        $this->id_amostra = $id_amostra;
+    }
 
-		public function setCalcio($calcio){
-			$this->calcio = $calcio;
-		}
+    public function getAreia() {
+        return $this->areia;
+    }
 
-		public function getFosforo(){
-			return $this->fosforo;
-		}
+    public function setAreia($areia) {
+        $this->areia = $areia;
+    }
 
-		public function setFosforo($fosforo){
-			$this->fosforo = $fosforo;
-		}
+    public function getSilte() {
+        return $this->silte;
+    }
 
-		public function getMateria_organica(){
-			return $this->materia_organica;
-		}
-		
-		public function setMateria_organica($materia_organica){
-			$this->materia_organica = $materia_organica;
-		}
+    public function setSilte($silte) {
+        $this->silte = $silte;
+    }
 
-		public function getPotencial_hidrogenionico(){
-			return $this->potencial_hidrogenionico;
-		}
-		
-		public function setPotencial_hidrogenionico($potencial_hidrogenionico){
-			$this->potencial_hidrogenionico = $potencial_hidrogenionico;
-		}
+    public function getArgila() {
+        return $this->argila;
+    }
 
-		public function getPotassio(){
-			return $this->potassio;
-		}
+    public function setArgila($argila) {
+        $this->argila = $argila;
+    }
 
-		public function setPotassio($potassio){
-			$this->potassio = $potassio;
-		}
+    public function getAluminio() {
+        return $this->aluminio;
+    }
 
-		public function getBoro(){
-			return $this->potassio;
-		}
+    public function setAluminio($aluminio) {
+        $this->aluminio = $aluminio;
+    }
 
-		public function setBoro($boro){
-			$this->boro = $boro;
-		}
+    public function getSodio() {
+        return $this->sodio;
+    }
+
+    public function setSodio($sodio) {
+        $this->sodio = $sodio;
+    }
+
+    public function getZinco() {
+        return $this->zinco;
+    }
+
+    public function setZinco($zinco) {
+        $this->zinco = $zinco;
+    }
+
+    public function getMaganes() {
+        return $this->maganes;
+    }
+
+    public function setMaganes($maganes) {
+        $this->maganes = $maganes;
+    }
+
+    public function getFerro() {
+        return $this->ferro;
+    }
+
+    public function setFero($ferro) {
+        $this->ferro = $ferro;
+    }
+
+    public function getCobre() {
+        return $this->cobre;
+    }
+
+    public function setCobre($cobre) {
+        $this->cobre = $cobre;
+    }
+
+    public function getAcidez_total() {
+        return $this->calcio;
+    }
+
+    public function setAcidez_total($acidez_total) {
+        $this->acidez_total = $acidez_total;
+    }
+
+    public function getMagnesio() {
+        return $this->magnesio;
+    }
+
+    public function setMagnesio($magnesio) {
+        $this->magnesio = $magnesio;
+    }
+
+    public function getCalcio() {
+        return $this->calcio;
+    }
+
+    public function setCalcio($calcio) {
+        $this->calcio = $calcio;
+    }
+
+    public function getFosforo() {
+        return $this->fosforo;
+    }
+
+    public function setFosforo($fosforo) {
+        $this->fosforo = $fosforo;
+    }
+
+    public function getMateria_organica() {
+        return $this->materia_organica;
+    }
+
+    public function setMateria_organica($materia_organica) {
+        $this->materia_organica = $materia_organica;
+    }
+
+    public function getPotencial_hidrogenionico() {
+        return $this->potencial_hidrogenionico;
+    }
+
+    public function setPotencial_hidrogenionico($potencial_hidrogenionico) {
+        $this->potencial_hidrogenionico = $potencial_hidrogenionico;
+    }
+
+    public function getPotassio() {
+        return $this->potassio;
+    }
+
+    public function setPotassio($potassio) {
+        $this->potassio = $potassio;
+    }
+
+    public function getBoro() {
+        return $this->potassio;
+    }
+
+    public function setBoro($boro) {
+        $this->boro = $boro;
+    }
+
 }
 
 ?>
--- src/classes/entity/usuario.php
+++ src/classes/entity/usuario.php
@@ -1,37 +1,39 @@
 <?php
-	/**
-	 * @author Jhonas
-	 *
-	 */
-	class Usuario{
-		
-		private $id_usuario;
-		private $usuario;
-		private $senha;
-		
-		public function getId_usuario(){
-			return $this->id_usuario;
-		}
-		
-		public function setId_usuario($id_usuario){
-			$this->id_usuario = $id_usuario;
-		}
-		
-		public function getUsuario(){
-			return $this->usuario;
-		}
-		
-		public function setUsuario($usuario){
-			$this->usuario = $usuario;
-		}
-		
-		public function getSenha(){
-			return $this->senha;
-		}
-		
-		public function setSenha($senha){
-			$this->senha = $senha;
-		}
-	}
-	
+
+/**
+ * @author Jhonas
+ *
+ */
+class Usuario {
+
+    private $id_usuario;
+    private $usuario;
+    private $senha;
+
+    public function getId_usuario() {
+        return $this->id_usuario;
+    }
+
+    public function setId_usuario($id_usuario) {
+        $this->id_usuario = $id_usuario;
+    }
+
+    public function getUsuario() {
+        return $this->usuario;
+    }
+
+    public function setUsuario($usuario) {
+        $this->usuario = $usuario;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+}
+
 ?>
