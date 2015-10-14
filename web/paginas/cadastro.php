81fc707805ba2e7fd7097cea8f83d1aa5fb286da
jessbotelho <jessicabotelho8@gmail.com>
2015-10-13 21:26:58 -0300

    changes

--- web/paginas/cadastro.php
+++ web/paginas/cadastro.php
@@ -55,12 +55,12 @@
 					
                     <input type="button" value="Solicitar Cadastro" id="botao">
         		</div>
-        	</div>
-		</form>
-		<script type="text/javascript">
-			$(document).ready(function(){
-				$("#cnpj_cli").mask("99.999.999/9999-99");
-			   	$('#cep_cli').mask('99999-999');
+         	</div>
+ 		</form>
++		
+ 		<script type="text/javascript">
+ 			$(document).ready(function(){
+ 				$("#cnpj_cli").mask("99.999.999/9999-99");
 			   	$('#telefonefixo').mask('(99)9999-9999');
 			   	$('#telefoneCelular').mask('(99) 9 9999-9999');
 			}); 
